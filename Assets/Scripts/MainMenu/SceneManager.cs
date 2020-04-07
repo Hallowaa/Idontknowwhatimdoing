eea6792ae6e55ac4dbaaceec006cdcbfe4162d32
Hallowa <d4n1el1t0mk@gmail.com>
2020-04-07 21:46:23 +0200

    aslidngwse

--- Assets/Scripts/MainMenu/SceneManager.cs
+++ Assets/Scripts/MainMenu/SceneManager.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
+    [SerializeField] private Canvas mainCanvas;
+    [SerializeField] private Canvas settingsCanvas;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCanvas.enabled = true;
+        settingsCanvas.enabled = false;
     }
 
     // Update is called once per frame
@@ -21,5 +24,17 @@ public class SceneManager : MonoBehaviour
     public void GoToMainGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName: "MainGame");
+    }
+
+    public void GoToSettingsMenu()
+    {
+        Invoke("TurnOnSettingsCanvas", 1.30f);
+        Camera.main.GetComponent<Animator>().SetTrigger("TransferToSettings");
+        mainCanvas.enabled = false;
+    }
+    
+    private void TurnOnSettingsCanvas()
+    {
+        settingsCanvas.enabled = true;
     }
 }
